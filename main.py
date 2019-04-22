72dd6e0bca0daa2d18c7d25f1bd4c3c83df632d9
tsmcandrew <timothy.sean.mcandrew@gmail.com>
2019-04-21 23:22:26 -0500

    Finished up to Display Individual Entries Section

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-#from hashutils import make_pw_hash, check_pw_hash
+from datetime import datetime 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#app.secret_key = 'y337kGcys&zP3B'
+
 
 
 class Blog(db.Model):
@@ -16,12 +15,15 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     blog_title = db.Column(db.String(120))
     blog_body = db.Column(db.String(500))
+    blog_date = db.Column(db.DateTime, nullable=False,
+        default=datetime.utcnow)
     
 
     def __init__(self, blog_title, blog_body):
        
         self.blog_title = blog_title 
         self.blog_body = blog_body 
+        
 
 
 """class NewBlog(db.Model):
@@ -92,6 +94,7 @@ def index():
     if request.method == 'POST':
         blog_title = request.form['blog_title']
         blog_body = request.form['blog_body']
+        #blog_date = request.form['blog_date']
         newpost = Blog(blog_title, blog_body)
         db.session.add(newpost)
         db.session.commit()
@@ -103,20 +106,35 @@ def index():
 @app.route('/newpost.html', methods=['GET', 'POST'])
 def newpost(): 
     new_blog = []
+    title_error = '' 
+    blog_error = '' 
 
     if request.method == 'POST':
         blog_title = request.form['blog_title']
         blog_body = request.form['blog_body']
+        #blog_date = request.form['blog_date']
         newpost = Blog(blog_title, blog_body)
         db.session.add(newpost)
         db.session.commit()
         new_blog.append(blog_title)
         new_blog.append(blog_body)
-        existing_blogs = Blog.query.all()
+        newest_blog = Blog.query.order_by(Blog.blog_date).limit(1).all
 
-        return render_template('/blog.html', existing_blogs=existing_blogs)
-    
-    return render_template('/newpost.html', new_blog=new_blog)
+        if blog_title == '' or blog_body == '': 
+            title_error = 'Oops! You forgot to input a Title for your piece.'
+            blog_error = 'Oops! You forgot to jot down your next masterpiece.'
+
+            return render_template(blog_title=blog_title, blog_body=blog_body, 
+            newest_blog=newest_blog, title_error=title_error, body_error=body_error)
+
+        #existing_blogs = Blog.query.all()
+        return redirect('/blog.html') #, existing_blogs=existing_blogs)
+# add code to test if blog entry, body & title, are empty. Return error message and newpost form. 
+
+    else:  
+        
+        newest_blog = Blog.query.order_by(Blog.blog_date).limit(1).all
+        return render_template('/newpost.html', newest_blog=newest_blog)
 
 
 if __name__ == '__main__':
